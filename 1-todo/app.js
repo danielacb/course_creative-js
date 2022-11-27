11767ab848ae7496021e4e42ea860d42b9a79836
Daniela Barbosa <contato@danielacb.com>
2022-11-27 19:46:30 -0300

    todo: add deleteCheck function

--- 1-todo/app.js
+++ 1-todo/app.js
@@ -3,6 +3,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 function addTodo(event) {
   event.preventDefault();
@@ -28,4 +29,19 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
 
   todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", () => todo.remove());
+  }
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
